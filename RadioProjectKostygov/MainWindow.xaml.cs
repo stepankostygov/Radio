e3c1cb2edce1fdc77a239d52eab6494ee53747d6
stepankostygov <stepan.kostygov@gmail.com>
2026-05-16 03:08:54 +0300

    Add files via upload

---
+++ RadioProjectKostygov/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using NAudio.Wave;
+
+namespace RadioPlayer
+{
+    public partial class MainWindow : Window
+    {
+        private IWavePlayer waveOut;
+        private MediaFoundationReader reader;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string url = UrlBox.Text;
+
+                reader = new MediaFoundationReader(url);
+                waveOut = new WaveOut();
+
+                waveOut.Init(reader);
+                waveOut.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            waveOut?.Stop();
+
+            reader?.Dispose();
+            waveOut?.Dispose();
+        }
+    }
+}
